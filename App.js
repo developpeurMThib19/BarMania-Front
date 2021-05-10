8da6624c1bddb92969fb4485d414c4c815d77626
kameronramah <kramaherison@gmail.com>
2021-05-10 15:34:23 +0200

    inscription

--- App.js
+++ App.js
@@ -2,21 +2,26 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Inscription from './app/components/inscription';
+
+export default class App extends React.Component {
+  render(){
+    return (
+      <View style={styles.container}>
+        <Inscription/>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#7FFF00',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingLeft: 60,
+    paddingRight: 60,
   },
 });
 
---
+++ app/components/inscription.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    TouchableOpacity,
+} from 'react-native';
+
+export default class Inscription extends React.Component {
+    render(){
+        return(
+            <View style={styles.inscription}>
+                <Text>testing</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    inscription: {
+
+    },
+});
